14f6fcb88e0bb7a076414aefcec90aaab0ac70c6
Max <max.makkusuu@gmail.com>
2020-10-27 00:36:13 +0100

    added things for profile page

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var registerRouter = require('./routes/register');
+var profileRouter = require('./routes/profile');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
 app.use('/users', usersRouter);
+app.use('/profiles', profileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/profile.js
+++ routes/profile.js
@@ -1,16 +1,30 @@
 var path = require('path');
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "portfolio"
+});
 
 /* GET profile-list page. */
 router.get('/', function(req, res, next) {
-    res.render('profile-list', { data: req.session });
+    res.render('profile-list', { data: req.body });
 });
 
-/* GET profile-page of a user with given ID
+/* GET profile-page of a user with given userID
 *  STILL IN WORK! */
-router.get('/', function (req, res, next) {
-    res.render('profile-page', { data: req.session })
+router.get('/user/:userID', function (req, res, next) {
+    param = req.params.userID;
+
+    let sql = mysql.format("SELECT * FROM accounts WHERE id=?", [param]);
+    con.query(sql, (err, rows, next) => {
+        res.render('profile-page', { data: rows[0] })
+    });
 });
 
 
