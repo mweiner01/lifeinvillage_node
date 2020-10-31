33c2d051a4d5c0be7f52212c0cde802906f001b2
Max <max.makkusuu@gmail.com>
2020-10-31 18:05:26 +0100

    commented all javascript files

--- public/javascripts/user-menu.js
+++ public/javascripts/user-menu.js
@@ -1,5 +1,17 @@
+/*
+* Function to open a drowdown for loggedin users so they can use:
+*
+* - My Profile
+* - Settings
+* - Logout
+*
+* */
+
+// get the button and the menu
 let btn = document.getElementById('user-menu-button');
 let menu = document.getElementById('user-menu');
+
+// function if the user clicks on the button aka profile picture then toggle class "hidden" and make menu visible or not
 window.onload = function () {
     if (btn) {
         btn.addEventListener('click', function () {
@@ -7,18 +19,8 @@ window.onload = function () {
         });
     }
 
+    // if user resizes the window then add class "hidden" to the menu
     addEventListener('resize', function () {
         menu.classList.add("hidden");
-    });
-
-    addEventListener('online', function () {
-        if (onlineStatus === 0) {
-            alert("Du bist nun wieder online");
-            onlineStatus = 1;
-        }
-    });
-    addEventListener('offline', function () {
-        onlineStatus = 0;
-        alert("Du bist nun offline");
     });
 }
