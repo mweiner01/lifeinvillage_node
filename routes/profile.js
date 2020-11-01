097972fd4b3335f25a27a399baeddf0910a8fad5
Max <max.makkusuu@gmail.com>
2020-11-01 02:35:49 +0100

    added comments

--- routes/profile.js
+++ routes/profile.js
@@ -20,9 +20,9 @@ var con = mysql.createConnection({
 * This will allow to get in touch with specific users you want to.
 * *Maybe u want to build a community of the rich* :D */
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     req.session.current_url = '/profiles';
-    res.render('profile-list', { data: req.session });
+    res.render('profile-list', {data: req.session});
 });
 
 /* GET profile-page (normal) of a user with given username
@@ -34,21 +34,25 @@ router.get('/:username', function (req, res, next) {
     // two mysql queries to get user and posts of the user
     let sql = mysql.format("SELECT * FROM accounts WHERE username=?", [param]);
     let sql2 = mysql.format("SELECT * FROM account_posts WHERE post_username=?", [param]);
+    let sql3 = mysql.format("SELECT * FROM post_comments WHERE 1")
 
     // mysql query to fetch user data
     con.query(sql, (err, rows) => {
         // if there is no error continue
-        if(!err) {
+        if (!err) {
             // start second mysql query to fetch post data
             con.query(sql2, (err2, rows2) => {
                 // if there is no error with the second query too then continue
-                if(!err2) {
-                    // save current url in session variable and render profile page with session data, user data and post data
-                    req.session.current_url = '/profiles/' + req.params.username;
-                    res.render('profile-page', {
-                        data: req.session,
-                        mysql: rows[0],
-                        posts: rows2
+                if (!err2) {
+                    con.query(sql3, (err3, rows3) => {
+                        // save current url in session variable and render profile page with session data, user data and post data
+                        req.session.current_url = '/profiles/' + req.params.username;
+                        res.render('profile-page', {
+                            data: req.session,
+                            mysql: rows[0],
+                            posts: rows2,
+                            comments: rows3
+                        });
                     });
                 } else {
                     // print out the error in console and render error page
@@ -58,6 +62,25 @@ router.get('/:username', function (req, res, next) {
         } else {
             // print out the error in console and render error page
             console.log(err)
+        }
+    });
+});
+
+router.post('/:username/posts', function (req, res, next) {
+    comment = req.body.comment;
+    id = req.body.id;
+    username = req.session.username;
+
+    // two mysql query strings to get user with username and create user
+    var sql = mysql.format("INSERT INTO post_comments (post_id, comment_from, comment_content, comment_date) VALUES (?, ?, ?, ?)", [id, username, comment, "Heute"]);
+    con.query(sql, function (err, result) {
+        // if error throw err
+        if (err) {
+            console.log(err)
+        } else {
+
+            // render register-success page with session data
+            res.redirect('/profiles/' + req.params.username + '/posts')
         }
     });
 });
@@ -75,12 +98,12 @@ router.get('/:username/activity', (req, res, next) => {
     // start mysql query num1
     con.query(sql, (err, rows) => {
         // if there is no error continue
-        if(!err) {
+        if (!err) {
             // 2nd mysql query to fetch activity data like transactions or levelups
             let sql2 = mysql.format("SELECT * FROM silver_transactions WHERE transaction_from=? OR transaction_to=? OR minecraft_name=? ORDER BY DATE(activity_date)=DATE(NOW() AND id)", [rows[0].minecraft_name, rows[0].minecraft_name, rows[0].minecraft_name]);
             // start 2nd mysql query
             con.query(sql2, (err2, rows2) => {
-                if(!err2) {
+                if (!err2) {
                     // save current url in session variable and render profile-page-activity with session data, user data and activity data
                     req.session.current_url = '/profiles/' + req.params.username + '/activity';
                     res.render('profile-page-activity', {
@@ -110,25 +133,29 @@ router.get('/:username/posts', (req, res, next) => {
     // two mysql queries to get user and posts of the user
     let sql = mysql.format("SELECT * FROM accounts WHERE username=?", [param]);
     let sql2 = mysql.format("SELECT * FROM account_posts WHERE post_username=?", [param]);
+    let sql3 = mysql.format("SELECT * FROM post_comments WHERE 1")
 
     // mysql query to fetch data
     con.query(sql, (err, rows) => {
-        // if there is no error then continue
-        if(!err) {
-            // mysql query
+        // if there is no error continue
+        if (!err) {
+            // start second mysql query to fetch post data
             con.query(sql2, (err2, rows2) => {
-                if(!err2)
-                {
-                    // save current_url in session variable and render profile page with session data, user data and post data
-                    req.session.current_url = '/profiles/' + req.params.username;
-                    res.render('profile-page', {
-                        data: req.session,
-                        mysql: rows[0],
-                        posts: rows2
+                // if there is no error with the second query too then continue
+                if (!err2) {
+                    con.query(sql3, (err3, rows3) => {
+                        // save current url in session variable and render profile page with session data, user data and post data
+                        req.session.current_url = '/profiles/' + req.params.username;
+                        res.render('profile-page', {
+                            data: req.session,
+                            mysql: rows[0],
+                            posts: rows2,
+                            comments: rows3
+                        });
                     });
                 } else {
                     // print out the error in console and render error page
-                    console.log(err)
+                    console.log(err2)
                 }
             });
         } else {
@@ -137,8 +164,6 @@ router.get('/:username/posts', (req, res, next) => {
         }
     });
 });
-
-
 
 
 module.exports = router;
